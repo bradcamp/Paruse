bef42e2bd08b19096bae2aa6ede8e377ebad6ca1
Brad Camp - Istockhomes <bradleydcamp@gmail.com>
2026-02-07 15:45:50 -0800

    Add files via upload

---
+++ Paruse/ParuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParuseApp.swift
+//  Paruse
+//
+//  Created by Zoom on 2025-07-12.
+//
+
+import SwiftUI
+
+@main
+struct ParuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
